838856d5809a5ac672d57a2deaeaf63477f80763
Sameer Shaikh <sshaikh99201@gmail.com>
2023-08-14 21:21:20 +0530

    Some changes

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css';
-import alertLogo from './alert-logo.png'; // Replace with the actual path to your alert logo image
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function Card({ task }) {
   const priorityLabels = ['No priority', 'Low', 'Medium', 'High', 'Urgent'];
@@ -9,16 +10,19 @@ function Card({ task }) {
   return (
     <li className="card">
       <div className="card-header">
-        <div className={`priority ${priorityColors[task.priority]}`}>
+        <div className="priority" style={{ backgroundColor: priorityColors[task.priority] }}>
           {priorityLabels[task.priority]}
         </div>
         <div className="user">{task.userId}</div>
-        {task.isAlert && (
-          <div className="alert-icon">
-            <img src={alertLogo} alt="Alert" className="alert-logo" />
-            <span className="alert-text">Feature Request</span>
-          </div>
-        )}
+        <div className="alert-icon">
+          {/* {task.isFeatureRequest && ( */}
+            <>
+              <i className="fa-solid fa-exclamation alert-logo"></i>
+              {/* <span className="alert-text">Feature Request</span> */}
+            </>
+          {/* )} */}
+        </div>
+
       </div>
       <h3 className="card-title">{task.title}</h3>
       <p className="card-description">{task.tag.join(', ')}</p>
